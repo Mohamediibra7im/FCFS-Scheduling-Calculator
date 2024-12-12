22b3fbf721c072e86ab8101896e436c3cab74525
midoriya-2-0 <HNU.24.308.Muhammad-Abdulnabi@hnu.edu.eg>
2024-12-12 16:49:30 +0200

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <footer>
-          <p>© 2023 FCFS Scheduling Calculator. All rights reserved.</p>
+
+        <footer className={`fixed bottom-0 left-1/2 transform -translate-x-1/2 z-20 w-full p-4 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 ${geistSans.variable}`}>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 text-center w-full">
+            <p>Copyright © 2024 Mohammed Ibrahim. All Rights Reserved.</p>
+          </span>
         </footer>
       </body>
     </html>
